98face5164fb67528d3dba27fd5e023413055a50
BayLadyCoder <rachada_32@hotmail.com>
2018-11-06 23:45:33 -0500

    Understand the Constructor Property - .constructor

--- OOPjs.js
+++ OOPjs.js
@@ -71,7 +71,7 @@ Using the dog object, give it a method called sayLegs. The method should return 
 let dog = {
   name: "Spot",
   numLegs: 4,
-  sayLegs: function() {
+  sayLegs: function () {
     return "This dog has " + dog.numLegs + " legs.";
   }
 };
@@ -104,7 +104,7 @@ Modify the dog.sayLegs method to remove any references to dog. Use the duck exam
 let dog = {
   name: "Spot",
   numLegs: 4,
-  sayLegs: function() {
+  sayLegs: function () {
     return "This dog has " + this.numLegs + " legs.";
   }
 };
@@ -376,4 +376,46 @@ for (let property in beagle) {
   } else {
     prototypeProps.push(property);
   }
+}
+
+
+/*
+
+Object Oriented Programming: Understand the Constructor Property
+
+There is a special constructor property located on the object instances duck and beagle that were created in the previous challenges:
+
+let duck = new Bird();
+let beagle = new Dog();
+
+console.log(duck.constructor === Bird); //prints true
+console.log(beagle.constructor === Dog); //prints true
+Note that the constructor property is a reference to the constructor function that created the instance.
+
+The advantage of the constructor property is that it's possible to check for this property to find out what kind of object it is. Here's an example of how this could be used:
+
+function joinBirdFraternity(candidate) {
+  if (candidate.constructor === Bird) {
+    return true;
+  } else {
+    return false;
+  }
+}
+Note
+Since the constructor property can be overwritten (which will be covered in the next two challenges) it’s generally better to use the instanceof method to check the type of an object.
+
+
+Write a joinDogFraternity function that takes a candidate parameter and, using the constructor property, return true if the candidate is a Dog, otherwise return false.
+
+*/
+function Dog(name) {
+  this.name = name;
+}
+
+// Add your code below this line
+function joinDogFraternity(candidate) {
+  if (candidate.constructor === Dog)
+    return true;
+  else
+    return false;
 }
